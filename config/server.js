2cca6d9f6d822025dddfb6263463fcc183caefd2
Kabir Bose <kabirbose04@gmail.com>
2023-11-18 17:09:40 -0500

    fixed layouts.ejs

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 // third party libraries
 const express = require("express");
-const ejsLayouts = require("express-ejs-layouts");
+const expressLayouts = require("express-ejs-layouts");
 
 // imported modules
 const pagesRouter = require("../routes/pages");
@@ -8,13 +8,15 @@ const pagesRouter = require("../routes/pages");
 const app = express();
 
 // middleware
-app.use("/", pagesRouter);
-app.use(express.static("public"));
 app.use(express.json());
-app.set("view engine", "ejs");
-app.use(ejsLayouts);
+app.use(expressLayouts);
+app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/javascript", express.static(__dirname + "public/javascript"));
+app.set("view engine", "ejs");
+
+// routes
+app.use("/", pagesRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
