ba0991884bddf31ac77febdeaca90fa0d9ab66cc
Kabir Bose <kabirbose04@gmail.com>
2023-11-18 17:01:43 -0500

    updated file structure

--- config/server.js
+++ config/server.js
@@ -1,10 +1,21 @@
+// third party libraries
 const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+
+// imported modules
+const pagesRouter = require("../routes/pages");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// middleware
+app.use("/", pagesRouter);
+app.use(express.static("public"));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/javascript", express.static(__dirname + "public/javascript"));
 
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("Listening on port http://localhost:3000");
 });
