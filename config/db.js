66828372ae8912ef5abc766121515fa78597a50b
Kabir Bose <kabirbose04@gmail.com>
2023-11-20 19:01:02 -0500

    connected to db

---
+++ config/db.js
@@ -0,0 +1,3 @@
+const MONGO_URI = `mongodb+srv://zikbanking:incorrect@cluster0.bs5mnf6.mongodb.net/expenses?retryWrites=true&w=majority`;
+
+module.exports = MONGO_URI;
--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 // third party libraries
 const express = require("express");
+const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 
 // imported modules
 const pagesRouter = require("../routes/pages");
+const MONGO_URI = require("../config/db");
 
 const app = express();
 
@@ -14,6 +16,13 @@ app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/javascript", express.static(__dirname + "public/javascript"));
 app.set("view engine", "ejs");
+
+mongoose.connect(MONGO_URI);
+let mongoDB = mongoose.connection;
+mongoDB.on("error", console.error.bind(console, "Connection error"));
+mongoDB.once("open", () => {
+  console.log("MongoDB Connected");
+});
 
 // routes
 app.use("/", pagesRouter);
